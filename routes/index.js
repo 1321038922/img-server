8990f51922ffd8634822e63909acddb0a7bbe5f3
Cason <1321038922@qq.com>
2018-12-24 18:08:58 +0800

    3003

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const qiniu = require('qiniu')
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var accessKey = 'your access key';
+  var secretKey = 'your secret key';
+  var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+  var options = {
+    scope: 'yaoqi',
+    returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"url":"http://pk8e34pne.bkt.clouddn.com/$(key)"}'
+  };
+  var putPolicy = new qiniu.rs.PutPolicy(options);
+  var uploadToken=putPolicy.uploadToken(mac);
+  res.header('Access-Control-Allow-Origin','*')
+  res.json({
+    code:200,
+    data:uploadToken,
+    msg:'success'
+  })
+});
+
+module.exports = router;
